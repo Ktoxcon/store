385aaa0849ee568f5da24c309b76bc778febd131
Ktoxcon <kennethtoxro@gmail.com>
2024-12-22 22:30:02 -0600

    feat: adds customer middleware

---
+++ backend/src/middleware/customer.middleware.ts
@@ -0,0 +1,24 @@
+import { UserRoles } from "@store/constants/roles";
+import { NextFunction, Request, Response } from "express";
+import { JwtPayload } from "jsonwebtoken";
+
+export function CustomerMiddleware(
+  _request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    const session: JwtPayload = response.locals.session;
+
+    if (session.data.userRole !== UserRoles.CUSTOMER) {
+      response.status(403).send({ success: false, error: "Forbidden" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      response.status(403).send({ success: false, error: error.message });
+    }
+  }
+}

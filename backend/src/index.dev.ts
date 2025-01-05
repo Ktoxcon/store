1707b10466edfc5855c3053c86634b212950960e
Ktoxcon <kennethtoxro@gmail.com>
2025-01-05 11:12:46 -0600

    feat: adds root user creation in DEV

--- backend/src/index.dev.ts
+++ backend/src/index.dev.ts
@@ -1,5 +1,7 @@
 import "dotenv/config";
 
 import { start } from "@store/app/start";
+import { createRootUser } from "./lib/db/create-root";
 
 start();
+createRootUser();
---
+++ backend/src/lib/db/create-root.ts
@@ -0,0 +1,20 @@
+import { User } from "@store/models/user.model";
+import { hash } from "argon2";
+import { UserRoles } from "../constants/roles";
+
+export async function createRootUser() {
+  const password = await hash(process.env.ROOT_PASSWORD!);
+
+  const rootExists = await User.findByPk(1);
+
+  if (rootExists) return;
+
+  await User.create({
+    id: 1,
+    password,
+    name: "Root",
+    lastName: "",
+    email: "root",
+    role: UserRoles.ADMIN,
+  });
+}
